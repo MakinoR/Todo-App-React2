fe0527efb0f1dd5a8373c2c3886c2886b722a571
Omaki-R <84137770+Omaki-R@users.noreply.github.com>
2021-05-24 01:39:22 +0900

    frame

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,20 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return null;
+  return (
+    <>
+      <div>
+        <input placeholder="にゅうりょくしてね" />
+        <button>Add</button>
+      </div>
+      <div>
+        <ul>
+          <li>達成項目</li>
+          <button>Complete</button>
+          <button>Delete</button>
+        </ul>
+      </div>
+      <div></div>
+    </>
+  );
 };
