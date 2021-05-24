ebcb7459932e246c530c0913552106512854728f
Omaki-R <84137770+Omaki-R@users.noreply.github.com>
2021-05-25 06:25:22 +0900

    frame

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,33 @@ import "./styles.css";
 export const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="にゅうりょくしてね" />
         <button>Add</button>
       </div>
-      <div>
+
+      <div className="incomplete-area">
+        <p className="title">未完のリスト</p>
         <ul>
-          <li>達成項目</li>
-          <button>Complete</button>
-          <button>Delete</button>
+          <li>
+            <div className="list-row">
+              <p>未達成項目</p>
+              <button>Complete</button>
+              <button>Delete</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <div className="complete-area">
+        <p className="title">完了リスト</p>
+        <ul>
+          <li>
+            <div className="list-row">
+              <p>達成項目</p>
+              <button>return</button>
+            </div>
+          </li>
         </ul>
       </div>
       <div></div>
